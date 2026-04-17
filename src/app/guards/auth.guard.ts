2f97a6d843a65755442170b868a669a8bf7f819f
Jorge R <jorge.rosado7@outlook.com>
2026-04-17 19:33:47 +0200

    Añadimos un almacenamiento de roles que usaremos para filtrar a que rutas puedes entrar en base al rol que tengas

--- src/app/guards/auth.guard.ts
+++ src/app/guards/auth.guard.ts
@@ -1,6 +1,7 @@
 import { CanActivateFn, Router } from '@angular/router';
 import { inject } from '@angular/core';
 import { jwtDecode } from 'jwt-decode';
+import { AuthService } from '../services/auth/auth.service';
 
 export const authGuard: CanActivateFn = () => {
 
@@ -17,10 +18,8 @@ export const authGuard: CanActivateFn = () => {
     const isExpired = payload.exp * 1000 < Date.now();
 
     if (isExpired) {
-
       localStorage.removeItem('token');
       localStorage.removeItem('usuario');
-
       router.navigate(['/login']);
       return false;
     }
@@ -32,4 +31,17 @@ export const authGuard: CanActivateFn = () => {
     router.navigate(['/login']);
     return false;
   }
+};
+
+export const roleGuard = (rolesPermitidos: string[]): CanActivateFn => {
+  return () => {
+    const authService = inject(AuthService);
+    const router      = inject(Router);
+ 
+    if (authService.hasRol(rolesPermitidos)) {
+      return true;
+    }
+    router.navigate(['/dashboard']);
+    return false;
+  };
 };
