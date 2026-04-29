e4ba131469a89355a0b889090f6a648f76f1a8c4
Jorge R <jorge.rosado7@outlook.com>
2026-04-29 18:11:56 +0200

    despliegue

--- src/app/pages/dashboard/dashboard-layout/dashboard-layout.ts
+++ src/app/pages/dashboard/dashboard-layout/dashboard-layout.ts
@@ -7,15 +7,14 @@ import { AuthService } from '../../../services/auth/auth.service';
   selector: 'app-dashboard-layout',
   imports: [RouterModule],
   templateUrl: './dashboard-layout.html',
-  styleUrl: './dashboard-layout.css'
+  styleUrl: './dashboard-layout.css',
 })
 export class DashboardLayout {
-
   constructor(private authService: AuthService) {}
 
-logout() {
-  if (confirm('¿Seguro que quieres cerrar sesión?')) {
-    this.authService.logout();
+  logout() {
+    if (confirm('¿Seguro que quieres cerrar sesión?')) {
+      this.authService.logout();
+    }
   }
-}
 }
